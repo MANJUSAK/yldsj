b43cd7e3437ab9fe645ac48aca1442cef9e9aea3
严彬荣 <453420698@qq.com>
2017-08-31 22:53:59 +0800

    页面修改

--- src/main/webapp/js/hosts.js
+++ src/main/webapp/js/hosts.js
@@ -1,5 +1,8 @@
 $(function () {
-    window.sessionStorage.setItem("Host", "http://www.myfreedata.com.cn:81/ylcx");
-    window.sessionStorage.setItem("index_host_img", 'http://www.myfreedata.com.cn:81/ylcx');
-    window.sessionStorage.setItem('Host1', 'http://www.myfreedata.com.cn:81/');
+    /*window.sessionStorage.setItem("Host", "http://www.myfreedata.com.cn:81/ylcx");
+     window.sessionStorage.setItem("index_host_img", 'http://www.myfreedata.com.cn:81/ylcx');
+     window.sessionStorage.setItem('Host1', 'http://www.myfreedata.com.cn:81/');*/
+    window.sessionStorage.setItem("Host", "http://172.16.0.253:8080/ylcx");
+    window.sessionStorage.setItem("index_host_img", 'http://172.16.0.253:8080/ylcx');
+    window.sessionStorage.setItem('Host1', 'http://172.16.0.253:8080/');
 });
